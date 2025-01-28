420af9487b76a4abe4f75de9d7dfd0ff509c7c0c
Olisnot <oliverpm2007@gmail.com>
2025-01-28 21:22:15 +0000

    gtk start

--- shoppinglistanalyzer/src/main.rs
+++ shoppinglistanalyzer/src/main.rs
@@ -1,3 +1,45 @@
-fn main() {
-    println!("Hello, world!");
+use gtk4::prelude::*;
+use gtk4::{glib, Application};
+
+const APP_ID: &str = "org.gtk_rs.ShoppingListAnalyzer";
+
+fn main() -> glib::ExitCode {
+    // Create a new application
+    let app = Application::builder().application_id(APP_ID).build();
+    app.connect_activate(build_ui);
+
+    // Run the application
+    app.run()
+}
+
+fn build_ui(app: &Application) {
+    // Create a button with label and margins
+    let button = gtk4::Button::builder()
+        .label("Press me!")
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
+
+    let is_clicked = std::cell::Cell::new(false);
+    // Connect to "clicked" signal of `button`
+    button.connect_clicked(move |button| {
+        if is_clicked.get() {
+            button.set_label("Hello World!");
+        } else {
+            button.set_label("Press me!");
+        }
+        is_clicked.set(!is_clicked.get());
+    });
+
+    // Create a window
+    let window = gtk4::ApplicationWindow::builder()
+        .application(app)
+        .title("Shopping List Analyzer")
+        .child(&button)
+        .build();
+    // Present window
+
+    window.present();
 }

5cf3c23ca121814c477e28e58794607833d8ae7a
Olisnot <oliverpm2007@gmail.com>
2025-02-11 19:19:22 +0000

    flake update adding openssl

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
     in
     {
       devShells."${system}".default = pkgs.mkShell {
-        buildInputs = /* bash */ [ nvim pkgs.gtk4 pkgs.pkg-config ];
+        buildInputs = /* bash */ with pkgs; [ nvim gtk4 pkg-config openssl];
         nativeBuildInputs = with pkgs; [ rustc cargo gcc rustfmt clippy ];
         RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
 
